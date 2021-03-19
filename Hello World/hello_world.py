6ee2390c418db97bc2b09f2dac55248649a62cbe
Alinda <AlindaHeng@gmail.com>
2021-03-19 13:05:36 -0700

    Moved tips to bottom of python file.

--- Hello World/hello_world.py
+++ Hello World/hello_world.py
@@ -12,14 +12,6 @@ Running this application:
 - Note that this is a development server and not meant for deployment.
 - Check here to deploy: https://flask.palletsprojects.com/en/1.1.x/deploying/
 
-Note:
-Other users on a network cannot access your server. Server is launched in debugging
-mode where users can execute code on your computer. If you trust users on your network,
-run this when executing the Flask app:
->>> flask run --host=0.0.0.0
-
-This tells your OS to listen to all public IPs.
-
 """
 
 from flask import Flask
@@ -32,3 +24,21 @@ app = Flask(__name__)		# __name__ can change depending on whether the app
 							# visit http://127.0.0.1:5000/test to see this.
 def hello_world():
 	return "Hello World"
+
+"""
+Tips:
+- Other users on a network cannot access your server. Server is launched in debugging
+mode where users can execute code on your computer. If you trust users on your network,
+run this when executing the Flask app:
+>>> flask run --host=0.0.0.0
+
+This tells your OS to listen to all public IPs.
+
+- You can enable debug mode so the server will reload itself on code changes.
+Enable development features (+ debug mode) by exporting and setting the FLASK_ENV variable
+before starting the server.
+[NOTE: Never use the debugger on production machine as it is a security risk]
+>>> export FLASK_ENV=development
+>>> flask run
+
+"""
